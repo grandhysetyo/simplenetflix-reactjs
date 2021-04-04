b81ff2ecfcc7355f4c7006f1541ddff4484c0208
Grandhys Setyo Utomo <setyo.grandhys1@gmail.com>
2021-04-05 02:57:58 +0700

    get data api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
+import api from './api.js'
+
 import Movie from './components/Movie'
-function App() {
+function App() {  
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello World</h1>
-        <Movie title="Netflix Original" />
+        <Movie title="Netflix Original" url={api.fetchNetflixOriginals}/>
+        <Movie title="Trending Now" url={api.fetchTrending}/>
       </header>
     </div>
   );
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from '../axios'
 
 function Movie(props){
+     const [movies, setMovie] = useState([])
+     useEffect(()=>{
+        async function fetchData(){
+            const request = await axios.get(props.url)
+            setMovie(request.data.results)
+        }
+        fetchData()
+     }, [props.url])
+     console.log(movies)
     return(
         <div className ="row">
             <h2>{props.title}</h2>
             <div className ="poster">
-                <img src="" className ="img-poster"/>
+                <img src="" className ="img-poster" alt=""/>
             </div>
         </div>
     )
