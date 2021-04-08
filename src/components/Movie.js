cea5083b0a0dcc33fd8913563979103230cdb536
grandhysetyo <setyo.grandhys1@gmail.com>
2021-04-08 09:15:36 +0700

    add poster movie

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react'
 import axios from '../axios'
+import './Movie.css';
 
+const image_uri = 'https://image.tmdb.org/t/p/original'
 function Movie(props){
      const [movies, setMovie] = useState([])
      useEffect(()=>{
@@ -12,10 +14,17 @@ function Movie(props){
      }, [props.url])
      console.log(movies)
     return(
-        <div className ="row">
+        <div className ="poster">
             <h2>{props.title}</h2>
-            <div className ="poster">
-                <img src="" className ="img-poster" alt=""/>
+            <div className ="row">
+                {
+                    movies.map(movie => {
+                        return(
+                            <img src={`${image_uri}${movie.poster_path}`} className ="img-poster" alt={movie.name} key={movie.id} />
+                        )
+                    })
+                }
+                
             </div>
         </div>
     )
